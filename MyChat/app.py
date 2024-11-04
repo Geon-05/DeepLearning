b9fdf3ccbbe2cd49b179eb42900d44e80b3a8fe7
Geon05 <zqrc05@gmail.com>
2024-11-05 00:48:34 +0900

    20241105

--- MyChat/app.py
+++ MyChat/app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 from rag_functions import load_docs, create_vectorstore, create_rag_chain
-import os
-import openai
 
 st.title("RAG Q&A 시스템")
 # 사용자 입력
@@ -35,5 +33,5 @@ if topic and question:
 st.sidebar.title("소개")
 st.sidebar.info(
     "이 앱은 RAG(검색 증강 생성) 시스템을 시연합니다. "
-    "위키피디아를 지식 소스로 사용하고 OpenAI의 GPT 모델을 통해 답변을 생성합니다."
+    "위키피디아를 지식 소스로 사용하고 Gemini 모델을 통해 답변을 생성합니다."
 )
--- MyChat/rag_functions copy.py
+++ MyChat/rag_functions copy.py
@@ -1,20 +1,19 @@
 import os
-import json
-import streamlit as st
-
 from langchain_community.document_loaders import WikipediaLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
+from langchain_openai import openai
+from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
-
-import google.generativeai as genai
+import json
 
 # 파일 읽기
 with open(r'D:\OneDrive\human\port-folio\personal_project\DeepLearning\day00_preparation\data\APIkey.json', 'r') as file:
     data = json.load(file)  # JSON 파일을 파이썬 딕셔너리로 로드
 
 # 환경 변수에서 API 키 가져오기
-api_key = data['Gemini']
+api_key = data['OpenAI']
 
 os.environ["OPENAI_API_KEY"] = api_key
 
@@ -31,30 +30,32 @@ def load_docs(query):
     return splits
 
 def create_vectorstore(splits):
-    embeddings = genai.get_embeddings
+    embeddings = OpenAIEmbeddings()
     vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
     return vectorstore
 
 def create_rag_chain(vectorstore):
-    # Gemini 모델 사용
-    model = "gemini-1.5-pro"  # 원하는 Gemini 모델 선택
-
-    # 프롬프트 조정
-    prompt_template = """
-    지식베이스를 참고하여 질문에 대한 답변을 생성해주세요. 답변은 최대한 간결하고 명확하게 요약하며, 필요한 경우 핵심적인 증거를 제시해주세요.
-    **지식베이스:** {context}
-    **질문:** {question}
-    **답변:**
-    """
+    llm = openai(model_name="gpt-3.5-turbo", temperature=0)
+    
+    prompt_template = """아래의 문맥을 사용하여 질문에 답하십시오.
+    만약 답을 모른다면, 모른다고 말하고 답을 지어내지 마십시오.
+    최대한 세 문장으로 답하고 가능한 한 간결하게 유지하십시오.
+    {context}
+    질문: {question}
+    유용한 답변:"""
+    
     PROMPT = PromptTemplate(
         template=prompt_template, input_variables=["context", "question"]
     )
 
-    # Gemini API를 사용하여 체인 생성
-    chain = genai.create_chain(
-        llm=model,
-        prompt=PROMPT,
-        vectorstore=vectorstore
+    chain_type_kwargs = {"prompt": PROMPT}
+    
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vectorstore.as_retriever(),
+        chain_type_kwargs=chain_type_kwargs,
+        return_source_documents=True
     )
-
-    return chain
+    
+    return qa_chain
